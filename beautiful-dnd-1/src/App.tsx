8253607585ba094d7453fe58d69ce6d0c79336ed
zehrakurt <153917389+zehrakurt@users.noreply.github.com>
2024-05-31 03:01:50 +0300

    a
    a

--- beautiful-dnd-1/src/App.tsx
+++ beautiful-dnd-1/src/App.tsx
@@ -6,6 +6,9 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
+import { nanoid } from 'nanoid';
+import { MdDeleteForever } from "react-icons/md";
+import { GrUpdate } from "react-icons/gr";
 const grid = 8;
 const CustomInputGroup = styled(InputGroup)`
   width: 400px; /* Burada istediğiniz genişliği belirleyin */
@@ -13,8 +16,8 @@ const CustomInputGroup = styled(InputGroup)`
 const CustomDiv = styled.div`
 text-align: center;
 `;
-const ListCon = styled.div`
-  width: 200px;
+const QuoteItem = styled.div`
+  width: 300px;
   border: 1px solid grey;
   margin-bottom: ${grid}px;
   background-color: lightblue;
@@ -30,11 +33,24 @@ const [newTodo,setNewTodo]=useState('');
 
 const onDragEnd=(result:any)=>{
 if (!result.destination)return;
-const TodoContent=[quotes];
+const TodoContent=Array.from(quotes);;
 const [removedTodo]=TodoContent.splice(result.source.index,1);
-TodoContent.splice(result.destination.index,removedTodo);
+TodoContent.splice(result.destination.index,0,removedTodo);
 setQuotes(TodoContent);
 
+}
+const addButton=()=>{
+  if(!newTodo.trim()) return alert("please enter valid data");
+const addId=nanoid()
+const addTodo={
+  id:addId,
+  content:newTodo,
+}
+setQuotes([...quotes, addTodo]);
+setNewTodo("")
+}
+const removeTodo=(todoId:string)=>{
+  setQuotes([...quotes.filter((quote)=>quote.id !== todoId)]);
 }
 
   return (
@@ -50,20 +66,39 @@ setQuotes(TodoContent);
           aria-label="Default"
           aria-describedby="inputGroup-sizing-default"
         />
-        <Button onClick={onDragEnd} variant="outline-secondary" id="button-addon2">
+        <Button onClick={addButton} variant="outline-secondary" id="button-addon2">
           Button
         </Button>
       </CustomInputGroup> 
       </CustomDiv>
       
-        <ul>
-        {quotes.map((quote) => (
-         <ListCon>  <li key={quote.id}>{quote.content}</li></ListCon>
-        ))}
-      </ul>
-      
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId='list'>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {quotes.map(({ id, content }, index) => (
+                <Draggable key={id} draggableId={id} index={index}>
+                  {(provided) => (
+                    <QuoteItem
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {content}
+                     <button onClick={() => removeTodo(id)}><MdDeleteForever /> </button>
+                      <button><GrUpdate /></button>
+                    </QuoteItem>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   )
 }
+
 
 export default App
